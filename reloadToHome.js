511e2ce246d4ef4d143cd460293e46b172906ecd
Mark De Guzman <siopaomark@gmail.com>
2025-04-07 21:41:56 +0800

    added bg for the system, responsive banner and announcement

--- reloadToHome.js
+++ reloadToHome.js
@@ -1,7 +1,7 @@
 // JavaScript function to redirect to login.php after 3 seconds
 function redirectToHome() {
   setTimeout(function () {
-    window.location.href = "home.php";
+    window.location.href = "home.html";
   }, 3000); // 1000 milliseconds = 1 second
 }
 

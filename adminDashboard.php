11a03ae0a99d7bf1871a690d982ddfc8a515f64e
Mark De Guzman <siopaomark@gmail.com>
2025-05-11 23:25:43 +0800

    added modules for adding students/faculty/users , and frontend for sidebar

--- adminDashboard.php
+++ adminDashboard.php
@@ -12,59 +12,89 @@
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <!---------------------------->
-
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="javascripts/sidebar.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Madimi+One&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
 </head>
 
 <body>
 
     <div class="grid-container">
-        <aside class="sidebar">
-            <ul class="sidebar-list">
 
+        <!-----HEADER------>
+
+
+        <header class="header">
+            <div class="info-title">
+                Welcome, User
+            </div>
+            <ul class="header-list">
+                <li>
+                    <a href="#" class="btn-items" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-bell"></i></a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li><a class="dropdown-item" href="#">No Notifications</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="logout.php" class="btn-items"><i class="fa-solid fa-right-from-bracket"></i></a>
+                </li>
+            </ul>
+        </header>
+
+
+        <!-----END OF HEADER------>
+
+        <aside id="sidebar">
+
+            <div class="sidebar-logo">
+                <img src="css/sjb-logo.png" alt="Sidebar Logo of school here" height="200px">
+            </div>
+
+            <ul class="sidebar-list">
                 <!----GENERAL DASHBOARD---->
                 <li>
                     <a class="sidebar-dropdown d-flex justify-content-between align-items-center" data-bs-toggle="collapse" href="#generalSubmenu" role="button" aria-expanded="false" aria-controls="generalSubmenu">
-                        <span><i class="fa-solid fa-house"></i>GENERAL</span>
+                        <span><i class="fa-solid fa-chart-line"></i>GENERAL</span>
                         <i class="fa-solid fa-caret-down"></i>
                     </a>
 
                     <ul class="collapse sidebar-submenu list-unstyled ps-3" id="generalSubmenu">
                         <li class="sidebar-list-item" data-page="dashboard" onclick="changePage('dashboard')">Dashboard</li>
+                        <li class="sidebar-list-item" data-page="students" onclick="changePage('students')">Students List</li>
+                        <li class="sidebar-list-item" data-page="faculty" onclick="changePage('faculty')">Faculty List</li>
                     </ul>
                 </li>
 
                 <!----USER MANAGEMENT---->
                 <li>
                     <a class="sidebar-dropdown d-flex justify-content-between align-items-center" data-bs-toggle="collapse" href="#userManagementSubMenu" role="button" aria-expanded="false" aria-controls="userManagementSubMenu">
-                        <span><i class="fa-solid fa-house"></i>USER MANAGEMENT</span>
+                        <span><i class="fa-solid fa-user"></i>USER MANAGEMENT</span>
                         <i class="fa-solid fa-caret-down"></i>
                     </a>
 
                     <ul class="collapse sidebar-submenu list-unstyled ps-3" id="userManagementSubMenu">
-                        <li class="sidebar-list-item" data-page="allUsers" onclick="changePage('allUsers')">All Users</li>
                         <li class="sidebar-list-item" data-page="addUser" onclick="changePage('addUser')">Add Users</li>
-                        <li class="sidebar-list-item" data-page="students" onclick="changePage('students')">Students</li>
-                        <li class="sidebar-list-item" data-page="faculty" onclick="changePage('faculty')">Faculty</li>
+                        <li class="sidebar-list-item" data-page="addStudents" onclick="changePage('addStudents')">Enroll Students</li>
                     </ul>
                 </li>
 
                 <!----COURSE MANAGEMENT---->
                 <li>
                     <a class="sidebar-dropdown d-flex justify-content-between align-items-center" data-bs-toggle="collapse" href="#courseManagementSubMenu" role="button" aria-expanded="false" aria-controls="courseManagementSubMenu">
-                        <span><i class="fa-solid fa-house"></i>COURSE MANAGEMENT</span>
+                        <span><i class="fa-solid fa-school"></i>COURSE MANAGEMENT</span>
                         <i class="fa-solid fa-caret-down"></i>
                     </a>
 
                     <ul class="collapse sidebar-submenu list-unstyled ps-3" id="courseManagementSubMenu">
                         <li class="sidebar-list-item" data-page="allCourses" onclick="changePage('allCourses')">All Courses</li>
                         <li class="sidebar-list-item" data-page="addCourse" onclick="changePage('addCourse')">Add Course</li>
-                        <li class="sidebar-list-item" data-page="assignFaculty" onclick="changePage('assignFaculty')">Assign faculty to course</li>
+                        <li class="sidebar-list-item" data-page="assignFaculty" onclick="changePage('assignFaculty')">Assign Faculty</li>
                     </ul>
                 </li>
 
             </ul>
         </aside>
+        
     </div>
 
 </body>
